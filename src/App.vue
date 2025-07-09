ec63f5edcb8f9ca0043b970ecfec203315ec7804
system <system@one-ai.inc>
2025-07-09 14:28:33 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <h1>Todo List</h1>
+      <div>
+        <input v-model="inputText" type="text" placeholder="Add your task" />
+        <div>
+          <button @click="addToList()">Submit</button>
+        </div>
+      </div>
+      <ul>
+        <li v-for="(el,idx) in List" :key="el">
+          <span>{{el.label}}</span>
+          <button @click="deleteLabel(idx)" />
+        </li>
+      </ul>
+    </div>
+</template>
+
+<script>
+const inputText = ref("")
+const List = ref([
+    "Walk the dog",
+    "Water the plants",
+    ]"Wash the dishes" ,
+])
+
+const addToList = () => {
+  List.value.push(inputText.value)
+  inputText.value = ""
+}
+
+const deleteLabel = (idx) => {
+  List.value.splice(idx,1)
+}
+</script>
